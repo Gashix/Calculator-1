6edc31b5ab331ac7158f92db9c30fc4ace3a6acf
Gashix <116107869+Gashix@users.noreply.github.com>
2022-10-23 16:26:21 +0100

    1 Program Calc
    Arabic + Rim (-10+10)(1...10)

---
+++ src/Calculator.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        //2+3
+        //X+V=XV
+        try{
+        Converter converter = new Converter();
+        String[] actions = {"+", "-", "/", "*"};
+        String[] regexActions = {"\\+", "-", "/", "\\*"};
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+        String exp = scn.nextLine();
+        exp=exp.replaceAll (" ","");
+
+
+        //Определяем арифметическое действие:
+        int actionIndex=-1;
+        for (int i = 0; i < actions.length; i++) {
+            if(exp.contains(actions[i])){
+                actionIndex = i;
+                break;
+            }
+        }
+        //Если не нашли арифметического действия, завершаем программу
+        if(actionIndex==-1){
+            System.out.println("Некорректное выражение");
+            return;
+        }
+
+        //Делим строчку по найденному арифметическому знаку
+        String[] data = exp.split(regexActions[actionIndex]);
+        //Определяем, находятся ли числа в одном формате (оба римские или оба арабские)
+        if(converter.isRoman(data[0]) == converter.isRoman(data[1])){
+            int a,b;
+            //Определяем, римские ли это числа
+            boolean isRoman = converter.isRoman(data[0]);
+            if(isRoman){
+                //если римские, то конвертируем их в арабские
+                //X+V
+                //x-10
+                //v - 5
+                a = converter.romanToInt(data[0]);
+                b = converter.romanToInt(data[1]);
+
+            }else{
+                //если арабские, конвертируем их из строки в число
+                a = Integer.parseInt(data[0]);
+                b = Integer.parseInt(data[1]);
+            }
+            //выполняем с числами арифметическое действие
+            if(-10<=a && a<11 && -10<=b && b<11){
+            int result;
+            switch (actions[actionIndex]){
+                case "+":
+                    result = a+b;
+                    break;
+                case "-":
+                    result = a-b;
+                    break;
+                case "*":
+                    result = a*b;
+                    break;
+                default:
+                    result = a/b;
+                    break;
+            }
+            //15->XV
+            if(isRoman){
+                //если числа были римские, возвращаем результат в римском числе
+                if(a>0 && b>0 && result>0)
+                System.out.println(converter.intToRoman(result));
+                else System.out.println("римские числа не могут быть отрицательными");
+            }
+            else{
+                //если числа были арабские, возвращаем результат в арабском числе
+                System.out.println(result);
+            }
+            } else System.out.println("калькулятор работает с числами от 1 до 10 включительно");
+        }else{
+            System.out.println("Числа должны быть в одном формате");
+        }
+
+
+    } catch (Throwable error){
+            System.out.println("Неверно введены данные");
+        }
+    }
+}
